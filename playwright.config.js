236cfd00f955be5f999e9a500d018f10ce6732ad
Diego Armando Chiri Torres <dgchiri@gmail.com>
2024-07-01 17:03:17 -0400

    rest

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,8 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:8080/',
+    url: 'http://127.0.0.1:8080',
+    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
